36591ba77b389ab3d890f42f58f88393cd2d80cb
Remwhat <89856020+Remwhat@users.noreply.github.com>
2023-09-20 18:08:51 +0100

    4 files modified

--- src/App.js
+++ src/App.js
@@ -4,15 +4,21 @@ import { Navbar } from './navbar.jsx';
 import './style.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import * as ReactBootstrap from 'react-bootstrap';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Redirect,
+} from 'react-router-dom';
 
 export default function App() {
   return (
     <div>
       <Navbar />
       <Router>
-        <Route path="/hotelsview/all" component={HotelsView}></Route>
-        <HotelsView></HotelsView>
+        <Route path="/hotelsview/:category" component={HotelsView}></Route>
+
+        <Redirect to="/hotelsview/all"></Redirect>
       </Router>
     </div>
   );
