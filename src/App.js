76041549f629ddb847d23897143cc7bab9afa10f
Remwhat <89856020+Remwhat@users.noreply.github.com>
2023-09-25 16:02:12 +0100

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
 import { HotelsView } from './hotelsview';
+
+import { HotelDetails } from './hoteldetails';
+
 import { Navbar } from './navbar.jsx';
 import './style.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import * as ReactBootstrap from 'react-bootstrap';
-import {
-  BrowserRouter as Router,
-  Route,
-  Link,
-  Redirect,
-} from 'react-router-dom';
+import { Router, Route, Switch, Link, Redirect } from 'react-router-dom';
 
 export default function App() {
   return (
     <div>
       <Navbar />
-      <Router>
+      <Switch>
         <Route path="/hotelsview/:searchNow?" component={HotelsView}></Route>
-        <Route path="/hoteldetails/:hotelID?" component={HotelDetails}></Route>
+        <Route path="/hoteldetails/:hotelID?" element={HotelDetails}></Route>
         <Redirect to="/hotelsview/all"></Redirect>
-      </Router>
+      </Switch>
     </div>
   );
 }
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import React, { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import { render } from 'react-dom';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import ReactDOM from 'react-dom/client';
+
+import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
 
-const root = createRoot(document.getElementById('app'));
+const app = ReactDOM.createRoot(document.getElementById('app'));
 
-root.render(
-  <StrictMode>
+app.render(
+  <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </StrictMode>
+  </React.StrictMode>
 );
